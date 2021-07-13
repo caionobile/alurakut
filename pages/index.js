e8d6183c3f25b244ac9cd780f8384bcaf21d9ec7
Caio Nóbile <caio.nobile51@gmail.com>
2021-07-12 23:44:37 -0300

    Primeira aula do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
-import styled from 'styled-components'
+import MainGrid from "@components/MainGrid";
+import Box from "@components/Box";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "@libs/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "@components/ProfileRelations";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const ProfileSidebar = ({ user }) => {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${user}.png`}
+        style={{ borderRadius: 8 }}
+      ></img>
+    </Box>
+  );
+};
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "caionobile";
+  const people = ["aline", "breno", "arthur"];
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar user={githubUser}></ProfileSidebar>
+        </div>
+        <div style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet></OrkutNostalgicIconSet>
+          </Box>
+        </div>
+        <div style={{ gridArea: "profileRelationsArea" }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Pessoas da comunidade</h2>
+
+            <ul>
+              {people.map((i) => {
+                return (
+                  <li>
+                    <a
+                      href={`https://api.github.com/users/${githubUser}/followers`}
+                      key={i}
+                    >
+                      <img src={`https://github.com/${githubUser}.png`}></img>
+                      <span>{i}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
